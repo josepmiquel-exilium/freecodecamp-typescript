e315b451d9a1e748a8c34981c472191accb05469
tommyshelby1917 <tommyshelby@disroot.org>
2023-01-16 09:51:35 +0100

    Arrays and unions

---
+++ 02-basics/myArray.ts
@@ -0,0 +1,20 @@
+const superheros: string[] = []
+const heroIds: Array<number> = []
+
+type User = {
+    name: string,
+    isActive: boolean
+}
+
+const allUsers: User[] = []
+
+const MLModels: number[][] = [
+    [255,255,255], [123131]
+]
+
+superheros.push('hola')
+heroIds.push(2)
+
+allUsers.push({name: 'josep', isActive: true})
+
+export {}
---
+++ 02-basics/myUnion.ts
@@ -0,0 +1,37 @@
+// Union types are used when a value can be more than a single type.
+// Such as when a property would be string or number.
+
+let score: string | number = 33
+
+score = true; // error
+
+type User = {
+    name: string,
+    id: number
+}
+
+type Admin = {
+    username: string,
+    id: number
+}
+
+let josep: User | Admin = {name: 'josep', id: 334}
+josep = {username: 'jm', id: 6969}
+
+function getDbId(id: number | string) {
+    // making some API calls
+    if(typeof id === 'string') id.toLowerCase()
+    if(typeof id === 'number') id + 2
+}
+
+getDbId(3)
+getDbId("3")
+
+// union in array
+const data: (number | string | boolean)[] = [1,2,3,4,'3', true]
+
+// allow only some values to a variable
+let seatPosition: "aisle" | "middle" | "window"
+seatPosition = 'back' // error because back is not allowed
+
+export {}
