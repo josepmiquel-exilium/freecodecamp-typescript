454876c8ce4974e55d9123d49920fd78897f7084
tommyshelby1917 <tommyshelby@disroot.org>
2023-01-19 17:28:29 +0100

    classes in typescript

--- 03-puretypescript/dist/index.js
+++ 03-puretypescript/dist/index.js
@@ -1,2 +1,32 @@
 "use strict";
-console.log('typescript is still here!'.toUpperCase());
+Object.defineProperty(exports, "__esModule", { value: true });
+// Classes
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this._courseCount = 1;
+        this.city = 'Brussels';
+        this.id = 1334;
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    // with a getter we can acces to and return private value
+    get getCourseCount() {
+        return this._courseCount;
+    }
+    // with a setter we can acces and change a value of a private instance
+    set setCourseCount(courseNum) {
+        if (courseNum <= 1)
+            throw new Error('Course should be more than 1');
+        this._courseCount = courseNum;
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+}
+const josep = new User('josepmiquel@exilium,cat', 'Josep');
+josep.city = 'Anvers'; // error bc is readonly
+josep.id; // error bc is private and you cant read it
+josep.deleteToken(); // error bc is private and you can only use inside the class
--- 03-puretypescript/src/index.ts
+++ 03-puretypescript/src/index.ts
@@ -1,1 +1,40 @@
-console.log('typescript is still here!'.toUpperCase())
+// Classes
+class User {
+    private _courseCount = 1
+
+    readonly city: string = 'Brussels'
+    private id: number = 1334
+
+    constructor(public email: string, public name: string) {
+    }
+
+    get getAppleEmail(): string {
+        return `apple${this.email}`
+    }
+
+    // with a getter we can acces to and return private value
+    get getCourseCount(): number {
+        return this._courseCount
+    }
+
+    // with a setter we can acces and change a value of a private instance
+    set setCourseCount(courseNum: number) {
+        if(courseNum <= 1) throw new Error('Course should be more than 1')
+        this._courseCount = courseNum
+
+    }
+
+    private deleteToken() {
+        console.log("Token deleted")
+    }
+
+}
+
+const josep = new User('josepmiquel@exilium,cat', 'Josep')
+
+josep.city = 'Anvers' // error bc is readonly
+josep.id // error bc is private and you cant read it
+
+josep.deleteToken() // error bc is private and you can only use inside the class
+
+export {}
